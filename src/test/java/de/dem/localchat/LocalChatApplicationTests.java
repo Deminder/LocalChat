9268500e3ccc2dcedd91a33681a1bba9d6b69e17
Deminder <tremminder@gmail.com>
2020-07-27 21:28:11 +0200

    initial commit

---
+++ src/test/java/de/dem/localchat/LocalChatApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dem.localchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
