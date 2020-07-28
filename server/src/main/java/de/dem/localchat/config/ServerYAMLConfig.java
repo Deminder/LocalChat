d8ee7f21c0fdccacc6e0b150e97a56a13dd3f022
Deminder <tremminder@gmail.com>
2020-07-28 17:13:00 +0200

    logic and server module split

---
+++ server/src/main/java/de/dem/localchat/config/ServerYAMLConfig.java
@@ -0,0 +1,11 @@
+package de.dem.localchat.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class ServerYAMLConfig {
+}
