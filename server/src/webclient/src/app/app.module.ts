0cd0dd9f4f746074ac1bafd90fb42bb74e2ac94c
Deminder <tremminder@gmail.com>
2020-08-09 18:24:02 +0200

    prettier

--- server/src/webclient/src/app/app.module.ts
+++ server/src/webclient/src/app/app.module.ts
@@ -1,28 +1,25 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ConversationComponent} from './conversation/conversation.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import { MatButtonModule} from '@angular/material/button';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConversationComponent } from './conversation/conversation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConversationComponent
-  ],
+  declarations: [AppComponent, ConversationComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
