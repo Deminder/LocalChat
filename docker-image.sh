99c36fbdf545e129f87e7f16298c5969f66f8aab
Deminder <tremminder@gmail.com>
2020-11-04 16:35:47 +0100

    add server-dev.env file

--- docker-image.sh
+++ docker-image.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-./gradlew build
-docker build -t 'localchat2'
+./gradlew build || exit 1
+
+if [ "$1" = "prod" ];then
+	cp server-prod.env server.env
+else
+	cp server-dev.env server.env
+fi
+docker build -t 'localchat2' .
--- podman-image.sh
+++ podman-image.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-./gradlew build
+./gradlew build || exit 1
+if [ "$1" = "prod" ];then
+	cp server-prod.env server.env
+else
+	cp server-dev.env server.env
+fi
 podman build -t 'localchat2' -f Dockerfile
