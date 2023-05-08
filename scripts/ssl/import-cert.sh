ed20daac26f280d7eef61008cbe6eb39d9470bb1
Deminder <tremminder@gmail.com>
2023-05-08 21:29:11 +0200

    ssl: Improve certificate generation

--- scripts/ssl/import-cert.sh
+++ scripts/ssl/import-cert.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-STORENAME=server-prod.p12
+CERT_FILE=$1
+STORENAME=${2:-server-prod}
 ALIAS=localchat
 read -sp "storepass:" pass 
 echo
 export STOREPASS=$pass
 keytool -importcert -v \
 	-alias "$ALIAS" \
-	-keystore "$STORENAME" \
+	-keystore "$STORENAME".p12 \
 	-storepass:env STOREPASS \
-	-file "$1" && echo "Imported certificate $STORENAME:$ALIAS from $1 ( move to server resource classpath:/ )"
+	-file "$CERT_FILE" && echo "Imported certificate $STORENAME:$ALIAS from $CERT_FILE ( move to server resource classpath:/ )"
 
--- scripts/ssl/init-keystore.sh
+++ scripts/ssl/init-keystore.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 # "CN=hostname,OU=?,O=?,L=?,ST=?,C=?"
-DNAME=$1 
-STORENAME=${2:-server-prod}
+
+C_EXT="san=${SAN:-dns:${CN}}"
+STORENAME=${1:-server-prod}
 ALIAS=localchat
 read -sp "storepass:" pass 
 echo
 export STOREPASS=$pass
-if [ ! -z "$DNAME" ]; then
+if [ ! -f "$STORENAME".p12 ]; then
+	for dname in CN OU O L ST C
+	do
+		[ -z "${!dname}" ] && MISSING_DNAME="$MISSING_DNAME$dname=? "
+	done
+	if [ ! -z "${MISSING_DNAME}" ]
+	then
+			echo "Missing fields: $MISSING_DNAME"
+			exit 1
+	fi
 	keytool -genkeypair -v \
 		-alias "$ALIAS" \
 		-keyalg rsa \
 		-keysize 4096 \
 		-validity 365 \
 		-storetype pkcs12 \
-		-dname "$DNAME" \
+		-dname "CN=${CN},OU=${OU},O=${O},L=${L},ST=${ST},C=${C}" \
+		-ext "$C_EXT" \
 		-keystore "$STORENAME".p12 \
 		-storepass:env STOREPASS && echo "Gen keypair $STORENAME:$ALIAS ( self signed )"
 fi
@@ -21,10 +32,16 @@ if [ ! -f "$STORENAME".p12 ]; then
 	echo "Expected dname argument to create new keystore!"
 	exit 1
 fi
+if [ -z "${CN}" ]
+then
+		echo "Missing field: CN"
+		exit 1
+fi
 keytool -certreq \
 	-alias "$ALIAS" \
 	-keystore "$STORENAME".p12 \
 	-storepass:env STOREPASS \
+	-ext "$C_EXT" \
 	-file "$STORENAME".csr && echo "Created CSR for $STORENAME:$ALIAS ( pass to CA )"
 
 
